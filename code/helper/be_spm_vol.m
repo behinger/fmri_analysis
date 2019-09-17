b91bdba3817c43e125f18a86b8edd62e0b18030e
Benedikt Ehinger <behinger@uos.de>
2019-09-17 10:00:47 +0200

    deleted eerytihg trying again

--- code/helper/be_spm_vol.m
+++
@@ -1,4 +0,0 @@
-function out = be_spm_vol(file)
-file = be_niigz_to_nii(file);
-out = spm_vol(file);
-    
--- experiment/functions/setupPsychtoolbox.m
+++
@@ -1,3 +0,0 @@
-% Call Psychtoolbox-3 specific startup function:
-cd('.\..\..\Psychtoolbox_3011_x64');
-SetupPsychtoolbox;
