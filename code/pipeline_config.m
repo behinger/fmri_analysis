87149226d89a48efea106d906e7bdafd4c0acdc7
Benedikt Ehinger <benehi@mentat004.dccn.nl>
2019-10-16 13:09:32 +0200

    bugfix

--- code/pipeline_config.m
+++ code/pipeline_config.m
@@ -1,4 +1,4 @@
-function pipeline_config(cfg)
+function cfg = pipeline_config(cfg)
 % Functions for GRID/cluster evluation
 cfg.loopfun = @(cfg)['cd ' cfg.scriptdir ';export subjectlist="'  strjoin(cfg.subjectlist),'"; export bidsdir="' cfg.bidsdir '";'];
 cfg.gridpipe_long_4cpu = sprintf('| qsub -l "nodes=1:ppn=4,walltime=22:00:00,mem=12gb" -o %s',fullfile(cfg.bidsdir,'logfiles/'));
