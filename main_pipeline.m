b71faebcd47174cc35f6ed1c1b072d40007aeae6
Benedikt Ehinger <behinger@uos.de>
2019-10-16 12:32:28 +0200

    Update main_pipeline.m

--- main_pipeline.m
+++ main_pipeline.m
@@ -14,9 +14,9 @@ cfg.subjectlist = {'sub-91'};
 
 % Functions for GRID/cluster evluation
 cfg.loopfun = @(cfg)['cd ' cfg.scriptdir ';export subjectlist="'  strjoin(cfg.subjectlist),'"; export bidsdir="' cfg.bidsdir '";'];
-cfg.gridpipe_long_4cpu = sprintf('| qsub -l "nodes=1:ppn=4,walltime=22:00:00,mem=12gb" -o %s',fullfile(cfg.bidsdir,'logfiles'));
-cfg.gridpipe_long = sprintf('| qsub -l "nodes=1:walltime=22:00:00,mem=4GB,procs=1" -o %s',fullfile(cfg.bidsdir,'logfiles'));
-cfg.gridpipe_short = sprintf('| qsub -l "nodes=1:walltime=5:00:00,mem=4GB,procs=1" -o %s',fullfile(cfg.bidsdir,'logfiles'));
+cfg.gridpipe_long_4cpu = sprintf('| qsub -l "nodes=1:ppn=4,walltime=22:00:00,mem=12gb" -o %s',fullfile(cfg.bidsdir,'logfiles/'));
+cfg.gridpipe_long = sprintf('| qsub -l "nodes=1:walltime=22:00:00,mem=4GB,procs=1" -o %s',fullfile(cfg.bidsdir,'logfiles/'));
+cfg.gridpipe_short = sprintf('| qsub -l "nodes=1:walltime=5:00:00,mem=4GB,procs=1" -o %s',fullfile(cfg.bidsdir,'logfiles/'));
 
 
 cfg.loopeval = cfg.loopfun(cfg); % export variables and dirs
